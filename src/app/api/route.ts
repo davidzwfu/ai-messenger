c9433bbf12bb6c9384dac1a67fa905cb710f417a
David Fu <davidzwfu@gmail.com>
2024-07-19 10:49:30 +0400

    switch to gpt-4o-mini

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -14,9 +14,9 @@ export async function POST(request: NextRequest) {
   })
   const prompt = usersData[userId].prompt
   const completion = await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-4o-mini',
     temperature: 1,
-    max_tokens: 300,
+    max_tokens: 500,
     response_format: { type: 'json_object' },
     messages: [
       { role: 'system', content: prompt },
